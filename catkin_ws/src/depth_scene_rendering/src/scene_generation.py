1. Found bug with intrinsics matrix itself! Fixed! Focal length was wrong, should be in pixel units, BlenSor kinect_flength is in real world mm units. This fixes the projection of 3D pcd to 2D image plane in postprocess_scenes.cpp - object now is in center of image! Previously, using pcd organized point cloud before resulted in object offsetted to the side, not sure why. pcd organized image coordinates are apparently off.     2. Images are now cropped to 100x100. With the 3D-to-2D debugged, the cropped image now contains the object. The cropped blobbed heatmap now overlaps correctly with the cropped object image!!! This gives the image input triplet (depth, visible_heatmap, occluded_heatmap).

catkin_ws/src/depth_scene_rendering/src/scene_generation.py
@@ -116,7 +116,7 @@ kinect_max_dist = cam_info [2]
 #   (Cannot write YAML directly, as Blender Python does not have PyYAML)
 # Don't want NumPy format, because might load in C++.
 intrinsics_path = get_intrinsics_path ()
-np.savetxt (intrinsics_path, intrinsics, '%g')
+np.savetxt (intrinsics_path, intrinsics, '%f')
 print ('%sCamera intrinsics matrix written to %s%s' % (ansi_colors.OKCYAN,
   intrinsics_path, ansi_colors.ENDC))
 
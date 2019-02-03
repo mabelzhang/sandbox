12d40a8bb224ce70d1148ae06d8fd9b76c56ca3b
mabel.m.zhang@gmail.com <mabel.m.zhang@gmail.com>
2019-02-03 04:02:06 -0500

    drop test implementation complete

--- catkin_ws/src/grasp_drop_test/src/drop_test.py
+++ catkin_ws/src/grasp_drop_test/src/drop_test.py
@@ -11,15 +11,18 @@ import csv
 
 import numpy as np
 
+import rospy
 import rospkg
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, Point, Quaternion
 
 # Custom
 from grasp_collection.config_consts import worlds, world_to_object_base
 from grasp_collection.grasp_io import GraspIO
 from util.ansi_colors import ansi_colors as ansi
-from util.ros_util import call_trigger_rossrv, matrix_from_Pose
-from util.gazebo_util import spawn_model, delete_model, xml_replace_static
+from util.ros_util import call_trigger_rossrv, matrix_from_Pose, \
+  Pose_from_matrix
+from util.gazebo_util import spawn_model, delete_model, xml_replace_static, \
+  gravity_on, set_model_state, get_model_state
 
 # Local
 from config_consts import dae_suffix
@@ -78,12 +81,18 @@ def main ():
   pkg_path = rospack.get_path ('grasp_drop_test')
   models_path = os.path.join (pkg_path, 'models')
 
+  # Meters. If object dropped more than this amount, count grasp as failure.
+  # Assumption: Gripper is loaded at 1 meter height, object should also be
+  #   at similar height. Once dropped to the ground, it would have dropped
+  #   plenty more than this threshold.
+  DROP_DELTA_Z_THRESH = 0.3
+
 
 
   objs_to_collect = range (len (worlds))
 
   # Activate Robotiq
-  success, _ = call_trigger_rossrv ('robotiq_takktile/robotiq/activate')
+  srv_success, _ = call_trigger_rossrv ('robotiq_takktile/robotiq/activate')
 
   # For each object
   for w_i in range (len (objs_to_collect)):
@@ -92,14 +101,19 @@ def main ():
 
     world_fname = worlds [oid]
 
+    # Turn off gravity before loading object
+    srv_success = gravity_on (False)
+    if not srv_success:
+      print ('%sERROR: Failed to turn off gravity in Gazebo. Drop test will not work!%s' % (
+        ansi.FAIL, ansi.ENDC))
 
-    # Load object mesh into Gazebo, set static
+    # Load object mesh into Gazebo
     obj_base = world_to_object_base (world_fname)
     model_path = os.path.join (models_path, obj_base + dae_suffix)
     obj_xml = sdf_replace_stock (
       os.path.join (pkg_path, 'sdf', 'stock_dae.sdf'), model_path)
-    spawn_model (obj_base, obj_xml, 'objects', Pose ())
-    # TODO call gazebo srv to set object as static
+    spawn_model (obj_base, obj_xml, 'objects', Pose (Point (1, 0, 1),
+      Quaternion ()))
 
 
     # Load grasp poses from file
@@ -107,41 +121,74 @@ def main ():
 
     drop_res = []
 
-
     for grasp in grasps:
+
+      # Reset scene
+
+      # Turn off gravity before opening gripper to release object
+      srv_success = gravity_on (False)
+      if not srv_success:
+        print ('%sERROR: Failed to turn off gravity in Gazebo. Drop test will not work!%s' % (
+          ansi.FAIL, ansi.ENDC))
+
+      # Open gripper
+      print ('Opening hand')
+      call_trigger_rossrv ('/robotiq_takktile/robotiq/open')
+
 
       # Move object to inverse matrix of grasp pose wrt object
       # Gripper pose wrt object
       gpose_mat = matrix_from_Pose (grasp.pose)
       # Object pose wrt gripper
       opose_mat = np.linalg.inv (gpose_mat)
-      # TODO call gazebo srv to set object pose
+      opose = Pose_from_matrix (opose_mat)
+      # Set object pose wrt gripper
+      srv_success = set_model_state (obj_base, opose, frame_id='robotiq')
 
- 
+      # Get current object height wrt world
+      obj_state = get_model_state (obj_base)
+      if not obj_state.success:
+        print ('%sERROR: Failed to get object state from Gazebo. Skipping object')
+        continue
+      obj_z = obj_state.pose.position.z
+
+
       # Close gripper
       print ('Closing hand')
       call_trigger_rossrv ('/robotiq_takktile/robotiq/close')
  
- 
-      # Set object to non-static, to be dropped if grasp is bad
-      # TODO call gazebo srv to set static to false
-      #   Look at gym_gazebo grasp_collection_pipeline.py
- 
+      # Turn on gravity, so that object will be dropped if grasp is bad
+      srv_success = gravity_on (True)
+      if not srv_success:
+        print ('%sERROR: Failed to turn off gravity in Gazebo. Drop test will not work!%s' % (
+          ansi.FAIL, ansi.ENDC))
+      # Wait for object to fall
+      rospy.sleep (1)
 
-      ''' 
-      # TODO: instead of this, just check object height, better validation
-      grasp_success, err_msg = call_trigger_rossrv (
-        '/robotiq_takktile/robotiq/check_grasp')
- 
- 
+
+      # Get current object height
+      obj_state = get_model_state (obj_base)
+      if not obj_state.success:
+        print ('%sERROR: Failed to get object state from Gazebo. Skipping object')
+        continue
+      new_z = obj_state.pose.position.z
+
+      # If dropped, new_z < obj_z. Subtract in direction to get positive number
+      if obj_z - new_z > DROP_DELTA_Z_THRESH:
+        grasp_success = False
+        print ('%sDetected grasp failure (pre-grasp z %g, post-grasp z %g)%s' % (
+          ansi.OKCYAN, obj_z, new_z, ansi.ENDC))
+      else:
+        grasp_success = True
+        print ('%sDetected grasp success (pre-grasp z %g, post-grasp z %g)%s' % (
+          ansi.OKCYAN, obj_z, new_z, ansi.ENDC))
       # Record drop test result
       drop_res.append (grasp_success)
-      '''
  
-      # Open gripper
-      print ('Opening hand')
-      call_trigger_rossrv ('/robotiq_takktile/robotiq/open')
-
+ 
+    # Open gripper
+    print ('Opening hand')
+    call_trigger_rossrv ('/robotiq_takktile/robotiq/open')
 
     # Remove object
     delete_model (obj_base)
@@ -149,8 +196,13 @@ def main ():
     DropTestIO.write_drop_result (os.path.basename (world_fname), drop_res)
 
 
+  # Print stats
+  n_grasp_success = np.count_nonzero (drop_res)
+  print ('%d grasps succeeded, %d failed' % (n_grasp_success,
+    len (drop_res) - n_grasp_success))
+
   # Deactivate Robotiq
-  success, _ = call_trigger_rossrv ('robotiq_takktile/robotiq/deactivate')
+  srv_success, _ = call_trigger_rossrv ('robotiq_takktile/robotiq/deactivate')
 
 
 
--- catkin_ws/src/robotiq_custom/robotiq_hand_description/robots/gen_sdf.sh
+++ catkin_ws/src/robotiq_custom/robotiq_hand_description/robots/gen_sdf.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [[ "$#" -ne 1 ]]; then
+  echo "Argument missing."
+  echo "Usage: gen_sdf.sh <filename_without_extension>"
+  return
+fi
+
 xacro --inorder $1.urdf.xacro > $1.urdf
 gz sdf --print $1.urdf > $1.sdf
 # Ref:  https://stackoverflow.com/questions/525592/find-and-replace-inside-a-text-file-from-a-bash-command
